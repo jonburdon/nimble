322ce6a2d7d1094351c2563b70e748eb8b3de03c
jonburdon <jb@burdonworld.net>
2019-06-25 15:18:25 +0100

    Add function to remove computers counters

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
 let whoseTurnItIs = 1;
 let countersTakenThisTurn = 0;
 let totalCounters = 21;
-let idealMoves = [1,2,3,1,1,2,3,1,1,2,3,1,1,2,3,1,1,2,3,1,1];
+let idealMoves = [1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1, 1];
 let mode = "hard";
 
 
@@ -26,10 +26,10 @@ function restartScores() {
 }
 
 function displayStartScores() {
-    reportScore("totalCounters",totalCounters),
-    reportScore("whoseTurn",whoseTurnItIs),
-    reportScore("gameStatus","Playing..."),
-    reportScore("takenThisTurn",countersTakenThisTurn);
+    reportScore("totalCounters", totalCounters),
+        reportScore("whoseTurn", whoseTurnItIs),
+        reportScore("gameStatus", "Playing..."),
+        reportScore("takenThisTurn", countersTakenThisTurn);
 };
 
 // -*- RUNS START OF GAME FUNCTIONS
@@ -40,12 +40,39 @@ function startGame() {
     displayStartScores();
 }
 
+// -*- Change difficulty mode
+
+function changeMode(choice) {
+    mode = choice;
+    console.log(`${mode} selected by changeMode function`)
+    reportScore("gameStatus", `Mode set to ${mode} mode.`)
+}
+
 // ----------------- Hiding and showing Counters on the screen  ----------------- 
 
 //-*- Remove Nth counter
-function hideNthCounter() {
-$(`.counter:nth-of-type(${totalCounters})`)
-.addClass("hidden");
+function hideNthCounter(counterNumber) {
+    $(`.counter:nth-of-type(${counterNumber})`)
+        .addClass("hidden");
+}
+
+//-*- Remove either one, two or three counters
+
+//*** SOMEWHERE HERE IT STOPS JAVA RUNNING ***/
+
+// function hideComputersCounters(numToTake) {
+//     // let countersToTake = numToTake;
+//     for (i = numToTake; i > 0; i --;) {
+         
+//         hideNthCounter(totalCounters-(i-1));
+//     }
+// }
+
+function hideComputersCounters(num) {
+    for (i = num; i > 0; i--) {
+        hideNthCounter(totalCounters-(i-1));
+      }
+
 }
 
 //-*- Show all counters
@@ -59,7 +86,7 @@ function showAllCounters() {
 
 // -*- increaseCountersTaken Function to increase number of counters taken in this turn    
 
-function increaseCountersTaken (counts) {
+function increaseCountersTaken(counts) {
     counts++;
     return counts;
 }
@@ -67,7 +94,7 @@ function increaseCountersTaken (counts) {
 
 // -*- decreaseOverallCounters Function to decrease number of overall counters
 
-function decreaseOverallCounters (counts) {
+function decreaseOverallCounters(counts) {
     counts--;
     return counts;
 };
@@ -86,12 +113,12 @@ function checkPassTurn(takenThisTurn) {
 
 // -*- checkSwitchPlayer Switches Player if checkPassTurn returns True
 
-function checkSwitchPlayer (currentPlayer) {
+function checkSwitchPlayer(currentPlayer) {
     let check = checkPassTurn(countersTakenThisTurn);
     if (check) {
-        return currentPlayer = (currentPlayer === 1)? 2:1;
+        return currentPlayer = (currentPlayer === 1) ? 2 : 1;
     } else {
-    return currentPlayer
+        return currentPlayer
     }
 }
 
@@ -105,7 +132,7 @@ function switchPlayer() {
 
 // -*- endTheGameOrContinue
 
-function endTheGameOrContinue () {
+function endTheGameOrContinue() {
     let endOrNot = checkForWin(totalCounters);
     if (endOrNot) {
         respondToWin();
@@ -115,16 +142,16 @@ function endTheGameOrContinue () {
 // -*- counterClicked - EXECUTE COUNTER CLICK FUNCTIONS - Above
 
 function counterIsClicked() {
-reportScore("whoseTurn", whoseTurnItIs);
-countersTakenThisTurn = increaseCountersTaken(countersTakenThisTurn);
-// reportScore("takenThisTurn",countersTakenThisTurn);
-totalCounters = decreaseOverallCounters(totalCounters);
-reportScore("totalCounters",totalCounters);
-switchPlayer();
-reportScore("whoseTurn", whoseTurnItIs);
-reportScore("takenThisTurn",countersTakenThisTurn);
-activateWinSequenceTest();
-humanOrComputer();
+    reportScore("whoseTurn", whoseTurnItIs);
+    countersTakenThisTurn = increaseCountersTaken(countersTakenThisTurn);
+    // reportScore("takenThisTurn",countersTakenThisTurn);
+    totalCounters = decreaseOverallCounters(totalCounters);
+    reportScore("totalCounters", totalCounters);
+    switchPlayer();
+    reportScore("whoseTurn", whoseTurnItIs);
+    reportScore("takenThisTurn", countersTakenThisTurn);
+    activateWinSequenceTest();
+    humanOrComputer();
 
 }
 
@@ -132,7 +159,7 @@ humanOrComputer();
 
 // -*- checkForWin
 
-function checkForWin (totalCounts) {
+function checkForWin(totalCounts) {
     if (totalCounts === 0) {
         return true;
     } else {
@@ -140,13 +167,13 @@ function checkForWin (totalCounts) {
     }
 }
 
-function respondToWin () {
+function respondToWin() {
     reportScore("gameStatus", "Game Over, Player " + whoseTurnItIs + " has Won!");
-    
+
     // Also need to clear screen etc, and update High Score tables etc
 }
 
-function activateWinSequenceTest () {
+function activateWinSequenceTest() {
     if (checkForWin(totalCounters)) {
         respondToWin();
     }
@@ -157,7 +184,7 @@ function activateWinSequenceTest () {
 
 // -*- checkPassAllowed Check if passing play to other player is allowed, and if so, swap whose turn it is
 
-function checkPassAllowed (passcheck) {
+function checkPassAllowed(passcheck) {
     console.log('checkPassAllowed initiated');
     console.log('whoseTurnItIs:' + whoseTurnItIs)
     if (passcheck > 0) {
@@ -169,20 +196,20 @@ function checkPassAllowed (passcheck) {
             console.log('whoseTurnItIs is not found to be 1, so return 1');
             return 1;
         }
-    } 
-    else {
+    } else {
         console.log('not found to be great than zero');
-        return whoseTurnItIs}
+        return whoseTurnItIs
+    }
 }
 
 // -*- passTurnToOtherPlayerManually - EXECUTE OTHER FUNCTIONS to update DOM by changing the player ONLY if allowed.
 
-function passTurnToOtherPlayerManually () { // Call this function when 'PASS' Button is clicked
-whoseTurnItIs = checkPassAllowed(countersTakenThisTurn);
-countersTakenThisTurn = 0;
-reportScore("whoseTurn",whoseTurnItIs);
-reportScore("takenThisTurn", countersTakenThisTurn);
-humanOrComputer();
+function passTurnToOtherPlayerManually() { // Call this function when 'PASS' Button is clicked
+    whoseTurnItIs = checkPassAllowed(countersTakenThisTurn);
+    countersTakenThisTurn = 0;
+    reportScore("whoseTurn", whoseTurnItIs);
+    reportScore("takenThisTurn", countersTakenThisTurn);
+    humanOrComputer();
 }
 
 // ----------------- Functions for Human Vs Computer  ----------------- 
@@ -192,65 +219,64 @@ humanOrComputer();
 function humanOrComputer() {
     console.log(`It is player ${whoseTurnItIs} 's turn according to HorC function`)
     if (mode !== "human" && whoseTurnItIs === 2) { // IF HUMAN UPDATE STATUS
-        
-                    if (mode === "impossible") {
-                        console.log('Impossible Mode activated in humanOrComputer')
-                        thinkThenMove(50);
-                    }
-                    else if (mode === "hard") {
-                        console.log('Hard Mode activated in humanOrComputer')
-                        thinkThenMove(8);
-                    }
-                    else if (mode === "medium") {
-                        console.log('Medium Mode activated in humanOrComputer')
-                        thinkThenMove(3);
-                    } 
-                    else { // DEFAULT TO EASY MODE
-                        console.log('Easy Mode activated in humanOrComputer')
-                        thinkThenMove(1);
-                     }
-        
+
+        if (mode === "impossible") {
+            console.log('Impossible Mode activated in humanOrComputer')
+            thinkThenMove(50);
+        } else if (mode === "hard") {
+            console.log('Hard Mode activated in humanOrComputer')
+            thinkThenMove(8);
+        } else if (mode === "medium") {
+            console.log('Medium Mode activated in humanOrComputer')
+            thinkThenMove(3);
+        } else { // DEFAULT TO EASY MODE
+            console.log('Easy Mode activated in humanOrComputer')
+            thinkThenMove(1);
         }
-        else // IF NOT HUMAN, TAKE A TURN BASED ON DIFFICULTY LEVEL
-        {
-            reportScore("gameStatus","Human Mode");
-        }
-    
+
+    } else // IF NOT HUMAN, TAKE A TURN BASED ON DIFFICULTY LEVEL
+    {
+        reportScore("gameStatus", "Human Mode");
+    }
+
 }
- 
+
 
 
 
 //-*- Impossible Mode - Computer makes ideal move every time
 
 function takeTheIdealMove() {
-    return idealMoves[totalCounters-1];
+    return idealMoves[totalCounters - 1];
 }
 
 //-*- Random Move logic
 
 // Pick a random number from 1 to 3 unless there are 2 counters left.
 function randomMoveWithThree() {
-    let guess3 = (Math.random()*3);
+    let guess3 = (Math.random() * 3);
     if (guess3 < 1) {
         return 1;
     } else if (guess3 < 2) {
         return 2;
-    } else {return 3}
+    } else {
+        return 3
+    }
 }
 
 // -*- Decide randomly between taking 1 or 2 counters
 
 function randomMoveWithTwo() {
     console.log('Using randomMoveWithTwo ...')
-    let guess2 = (Math.random()*2);
+    let guess2 = (Math.random() * 2);
     console.log(guess2)
-    if (guess2 <1) {
+    if (guess2 < 1) {
         console.log('I will return 1');
         return 1
     } else {
         console.log('I will return 2');
-        return 2}
+        return 2
+    }
 }
 
 //-*- Turn off Human Playability for 3 seconds whilst Computer 'thinks'
@@ -258,9 +284,9 @@ function randomMoveWithTwo() {
 function thinkThenMove(diffLevel) {
     console.log('Human move is off');
     reportScore("gameStatus", "I am thinking ...");
-    setTimeout(function(){
+    setTimeout(function () {
         levelledMove(diffLevel)
-       },3000);
+    }, 3000);
 }
 
 
@@ -282,38 +308,44 @@ function makeRandomMove(counters) {
 
 function levelledMove(difficulty) {
     console.log('Human is off');
-    
-myGo = (Math.random()*(difficulty+3)); //Pass this function 50 for impossible 8 for hard, 3 for Medium Mode, 1 for Easy Mode
-console.log(`levelledMove received ${difficulty} and will return ${myGo}`)
 
-if (myGo > difficulty) {
-    console.log('I chose to take a random move');
-    let dec = makeRandomMove(totalCounters); 
-    console.log(`I decided to take ${dec} counters`);
-    totalCounters = totalCounters - dec;
-    
-    reportScore("gameStatus","Computer Decided to take "+ dec + " counters.");
-    
-    reportScore("totalCounters", totalCounters);
-} else {
-    console.log('I chose to take an ideal move');
-    let dec = takeTheIdealMove(); 
-    console.log(`I decided to take ${dec} counters`)
-    totalCounters = totalCounters - dec;
-   
-    reportScore("gameStatus","Computer Decided to take "+ dec + " counters.");
-    
-    reportScore("totalCounters", totalCounters);
-    console.log('Human move is def back on');
-}
+    myGo = (Math.random() * (difficulty + 3)); //Pass this function 50 for impossible 8 for hard, 3 for Medium Mode, 1 for Easy Mode
+    console.log(`levelledMove received ${difficulty} and will return ${myGo}`)
 
-   // Pass play to player 1 UNLESS Computer has won.
-   if (totalCounters === 0) {
-    respondToWin();
-} else {
-    whoseTurnItIs = 1;
-    reportScore("whoseTurn", whoseTurnItIs);
-}
+    if (myGo > difficulty) {
+        console.log('I chose to take a random move');
+        let dec = makeRandomMove(totalCounters);
+        console.log(`I decided to take ${dec} counters`);
+
+          hideComputersCounters(dec);      
+        
+          totalCounters = totalCounters - dec;
+        
+        reportScore("gameStatus", "Computer Decided to take " + dec + " counters.");
+
+        reportScore("totalCounters", totalCounters);
+    } else {
+        console.log('I chose to take an ideal move');
+        let dec = takeTheIdealMove();
+        console.log(`I decided to take ${dec} counters`)
+        
+        hideComputersCounters(dec);
+
+        totalCounters = totalCounters - dec;
+
+        reportScore("gameStatus", "Computer Decided to take " + dec + " counters.");
+
+        reportScore("totalCounters", totalCounters);
+        console.log('Human move is def back on');
+    }
+
+    // Pass play to player 1 UNLESS Computer has won.
+    if (totalCounters === 0) {
+        respondToWin();
+    } else {
+        whoseTurnItIs = 1;
+        reportScore("whoseTurn", whoseTurnItIs);
+    }
 }
 
 
@@ -324,7 +356,7 @@ if (myGo > difficulty) {
 //     totalCounters = totalCounters - myDecision;
 //     reportScore("gameStatus","Computer Decided to take "+ myDecision + " counters.");
 //     reportScore("totalCounters", totalCounters);
-    
+
 //     // Pass play to player 1 UNLESS Computer has won.
 //     if (totalCounters === 0) {
 //         respondToWin();
@@ -372,24 +404,12 @@ if (myGo > difficulty) {
 
 function computersTurn() { // When working, must receive 50, 8, 3 or 1 for Impossible, hard, medium and easy
 
-    // Say 'I a thinking'
-
-    // Wait 2 seconds
-
-    // Carry on
-    
     let decision = makeRandomMove(totalCounters);
-    
-    // Take first counter away
-
-    // Take second counter away
-
-    // Take third counter away
 
     totalCounters = totalCounters - decision;
-    reportScore("gameStatus","Computer Decided to take "+ decision + " counters.");
+    reportScore("gameStatus", "Computer Decided to take " + decision + " counters.");
     reportScore("totalCounters", totalCounters);
-    
+
     // Pass play to player 1 UNLESS Computer has won.
     if (totalCounters === 0) {
         respondToWin();
@@ -403,11 +423,43 @@ function computersTurn() { // When working, must receive 50, 8, 3 or 1 for Impos
 
 // ----------------- jQuery Event Listeners  ----------------- 
 
-$(document).ready(function () {
-    $(".counter").click(function (e) { 
-        e.preventDefault();
-        counterIsClicked();
-        $(this).addClass('hidden');
-    });
-});
 
+
+$(document).ready(function () {
+
+
+
+    $(".counter").click(function (e) {
+        if (whoseTurnItIs === 2 && mode !== "human") {
+            reportScore("gameStatus", "Human Clicks are off");
+        } else {
+            e.preventDefault();
+            counterIsClicked();
+            $(this).addClass('hidden');
+        }
+    });
+
+    $("#impossiblemodebutton").click(function() {
+        changeMode("impossible");
+    });
+
+    $("#hardmodebutton").click(function() {
+        changeMode("hard");
+    });
+
+    $("#mediummodebutton").click(function() {
+        changeMode("medium");
+    });
+
+    $("#easymodebutton").click(function() {
+        changeMode("easy");
+    });
+
+    $("#humanmodebutton").click(function() {
+        changeMode("human");
+    });
+
+
+
+
+});
